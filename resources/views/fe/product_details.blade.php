df08f480b1ea97f2e688e48c758e4122b5fde364
LeviAKM2K4 <116139867+LeviAKM2K4@users.noreply.github.com>
2023-07-10 22:05:07 +0700

    j

--- resources/views/fe/product_details.blade.php
+++ resources/views/fe/product_details.blade.php
@@ -16,7 +16,7 @@
             </div>
             <div class="row">
 
-               
+
 
                 <div class="col-lg-6 col-md-6">
                     <div class="tab-content">
@@ -25,32 +25,32 @@
                                 <img src="{{ asset('/images/' . $prod->image) }}" alt="">
                             </div>
                             <div style="padding-left: 0px;" class="col-lg-3 col-md-3">
-                  <ul class="nav nav-tabs" role="tablist">
-                      <li style="display: inline-flex;"  class="nav-item">
-                          <a style="margin-right: 5px;" class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
-                              <div class="product__thumb__pic set-bg" data-setbg="{{ asset('/images/' . $prod->image) }}">
-                                
-                              </div>
-                          </a>
+                                <ul class="nav nav-tabs" role="tablist">
+                                    <li style="display: inline-flex;" class="nav-item">
+                                        <a style="margin-right: 5px;" class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
+                                            <div class="product__thumb__pic set-bg" data-setbg="{{ asset('/images/' . $prod->image) }}">
 
-                          <a style="margin-right: 5px;" class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
-                              <div class="product__thumb__pic set-bg" data-setbg="{{ asset('/images/' . $prod->image) }}">
-                                
-                              </div>
-                          </a>
+                                            </div>
+                                        </a>
 
-                          <a style="margin-right: 5px;" class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
-                              <div class="product__thumb__pic set-bg" data-setbg="{{ asset('/images/' . $prod->image) }}">
-                                
-                              </div>
-                          </a>
+                                        <a style="margin-right: 5px;" class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
+                                            <div class="product__thumb__pic set-bg" data-setbg="{{ asset('/images/' . $prod->image) }}">
 
-                         
+                                            </div>
+                                        </a>
+
+                                        <a style="margin-right: 5px;" class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
+                                            <div class="product__thumb__pic set-bg" data-setbg="{{ asset('/images/' . $prod->image) }}">
+
+                                            </div>
+                                        </a>
 
 
-                      </li>
-                  </ul>
-              </div> 
+
+
+                                    </li>
+                                </ul>
+                            </div>
 
                         </div>
 
@@ -88,7 +88,7 @@
                         <div class="product__details__last__option">
                             <h5><span>Guaranteed Safe Checkout</span></h5>
                             <img src="img/shop-details/details-payment.png" alt="">
-                           
+
                         </div>
                     </div>
                 </div>
@@ -101,37 +101,37 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="product__details__tab">
-                       
+
                         <div class="tab-content">
                             <div class="tab-pane active" id="tabs-5" role="tabpanel">
                                 <div class="product__details__tab__content">
-                                    <p class="note">Free Shop is one of the leading companies in interior design and construction. 
-                                        With enthusiasm, experience, enthusiasm and cosmetics, 
-                                        the Home&Home team confidently brings customers the appraised interior styles, 
+                                    <p class="note">Free Shop is one of the leading companies in interior design and construction.
+                                        With enthusiasm, experience, enthusiasm and cosmetics,
+                                        the Home&Home team confidently brings customers the appraised interior styles,
                                         enhancing the value of your home. With us, you will surely be satisfied with your living space.</p>
                                     <div class="product__details__tab__content__item">
                                         <h5>What is Interior Design?</h5>
-                                        <p>When it comes to interior design, many people are not clear about this concept. 
+                                        <p>When it comes to interior design, many people are not clear about this concept.
                                             Interior design is understood as the harmonious coordination between objects, colors, lighting, decorations,
-                                             architectural aesthetics and spiritual and feng shui elements to create a comfortable living environment for you.
-                                              , convenient.</p>
-                                        <p>Referring to interior design is referring to the synthesis of the whole sky of art, fine arts and science and technology. 
-                                            It can be said that interior design is a prerequisite in the construction of today's buildings. 
+                                            architectural aesthetics and spiritual and feng shui elements to create a comfortable living environment for you.
+                                            , convenient.</p>
+                                        <p>Referring to interior design is referring to the synthesis of the whole sky of art, fine arts and science and technology.
+                                            It can be said that interior design is a prerequisite in the construction of today's buildings.
                                             Typically, modern interior architecture emphasizes the harmonious arrangement of simple spatial shapes.</p>
                                     </div>
                                     <div class="product__details__tab__content__item">
                                         <h5>SERVICE</h5>
-                                        <p>Free Shop always listens to and keeps up with the prevailing trends: 
-                                            Indochine style, Neo Classic neoclassical style, 
-                                            Modern modern style are subtly applied by Icon designers in each residential space but still bearing the unique imprint of the owner's personality. 
-                                            Customer satisfaction after each project is handed over is a certificate of achievement and tireless efforts of our staff. 
+                                        <p>Free Shop always listens to and keeps up with the prevailing trends:
+                                            Indochine style, Neo Classic neoclassical style,
+                                            Modern modern style are subtly applied by Icon designers in each residential space but still bearing the unique imprint of the owner's personality.
+                                            Customer satisfaction after each project is handed over is a certificate of achievement and tireless efforts of our staff.
                                             Free Shop Interior Design Joint Stock Company has gradually become a professional interior designer & constructor serving a series of projects of villas,
-                                             townhouses, apartments, offices, restaurants, hotels, etc. resort…</p>
+                                            townhouses, apartments, offices, restaurants, hotels, etc. resort…</p>
                                     </div>
                                 </div>
                             </div>
-                           
-                            
+
+
                         </div>
                     </div>
                 </div>
