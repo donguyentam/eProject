5ee8d31289a05658b80c6dda427187fcc7fd81cc
LeviAKM2K4 <116139867+LeviAKM2K4@users.noreply.github.com>
2023-07-15 19:00:37 +0700

    aa

--- resources/views/fe/news/workroom.blade.php
+++ resources/views/fe/news/workroom.blade.php
@@ -36,24 +36,40 @@
 
                 <div class="row">
                     @foreach($products as $item)
-                    <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg">
-                                <img src="{{ asset('/images/'. $item->image) }}" alt="">
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                <div style="border: 2px solid black; padding-top: 5px;" class="product__item">
+                    <div class="product__item__pic set-bg">
+                        <img style="padding-left: 8px;" src="{{ asset('/images/'. $item->image) }}" alt="">
+                        <span class="label">New</span>
 
-                            </div>
-                            <div class="product__item__text">
-                                <h6>{{ $item->price }} VNĐ</h6>
-                                <a href="#" class="add-cart" data-pid="{{ $item->id }}">+ Add To Cart</a>
-
-
-                            </div>
-                            <h4>
-                                <a style="color: #0d0d0d;
-    font-weight: 700;" href="{{ Route('productDetails', $item->id) }}">{{ $item->name }}</a>
-                            </h4>
-                        </div>
                     </div>
+                    <div class="product__item__text">
+
+                        <h3><a style="margin-left: 20%; margin-top: -10%;" href="#" class="btn" data-pid="{{ $item->id }}">+ Add To Cart</a></h3>
+
+                        <!-- <div class="product__color__select">
+                          <label for="pc-1">
+                              <input type="radio" id="pc-1">
+                          </label>
+                          <label class="active black" for="pc-2">
+                              <input type="radio" id="pc-2">
+                          </label>
+                          <label class="grey" for="pc-3">
+                              <input type="radio" id="pc-3">
+                          </label>
+                      </div> -->
+                    </div>
+                    <h6 style="margin-top: 17px; text-align: center;">
+                        <p>{{ $item->price }} VND</p>
+                    </h6>
+                    <div>
+                        <h5 style="text-align: center; margin-bottom: 15px;">
+                            <a style="color: black;" href="{{ Route('productDetails', $item->id) }}"><b>{{ $item->name }}</b></a>
+                        </h5>
+                    </div>
+
+                </div>
+            </div>
                     @endforeach
 
                 </div>
